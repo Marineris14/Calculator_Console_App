c4dd613dcb4835bb5c8a1d32a8688d2a97410a39
DESKTOP-G6IS2T2\Toshiba <marineriscr@gmail.com>
2021-10-26 13:34:23 +0300

    Add Calc

---
+++ Calculator_App/Calculator_App/Calc.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleCalculator
+{
+    public static class Calc
+    {
+        static public double Sum(double x, double y)
+        {
+            return x + y;
+        }
+
+        static public double Minus(double x, double y)
+        {
+            return x - y;
+        }
+
+        static public double Multiply(double x, double y)
+        {
+            return x * y;
+        }
+
+        static public double RestOfDivide(double x, double y)
+        {
+            return x % y;
+        }
+
+        static public double Divide(double x, double y)
+        {
+            while (y == 0)
+            {
+                Console.WriteLine("Can not divide by zero");
+                return 0;
+            }
+            return x / y;
+        }
+
+        static public double Sqrt(double num)
+        {
+            return Math.Sqrt(num);
+        }
+
+        static public double NumInput()
+        {
+            string numberString = Console.ReadLine();
+            bool isDoubleInput = double.TryParse(numberString, out double numberDouble);
+            while (!isDoubleInput)
+            {
+                Console.Write("Wrong input! Try again: ");
+                numberString = Console.ReadLine();
+                isDoubleInput = double.TryParse(numberString, out numberDouble);
+            }
+            return numberDouble;
+        }
+
+        static public bool OperatorInput(out string mathOperator)
+        {
+            List<string> operatorWithSecondArgument = new List<string> { "+", "-", "*", "/", "%", "^" };
+            List<string> operatorWithOneArgument = new List<string> { "sqrt", "!" };
+            while (true)
+            {
+                mathOperator = Console.ReadLine();
+                if (operatorWithSecondArgument.Contains(mathOperator))
+                {
+                    return true;
+                }
+                else if (operatorWithOneArgument.Contains(mathOperator))
+                {
+                    return false;
+                }
+                Console.WriteLine();
+                Console.Write("Wrong input! Try again: ");
+            }
+        }
+    }
+}
---
+++ Calculator_App/Calculator_App/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Simple console calculator\n");
+            Console.Write("Input first operand: ");
+            double firstOperand = Calc.NumInput();
+            Console.WriteLine();
+            Console.Write("Input operator (+ - * / % sqrt !): ");
+            bool isSecondNeed = Calc.OperatorInput(out string mathOperator);
+            double secondOperand = 0;
+            if (isSecondNeed = true)
+            {
+                Console.WriteLine("Input second operand: ");
+                secondOperand = Calc.NumInput();
+                Console.WriteLine();
+            }
+            double result = 0;
+            switch (mathOperator)
+            {
+                case "+":
+                    result = Calc.Sum(firstOperand, secondOperand);
+                    break;
+                case "-":
+                    result = Calc.Minus(firstOperand, secondOperand);
+                    break;
+                case "*":
+                    result = Calc.Multiply(firstOperand, secondOperand);
+                    break;
+                case "/":
+                    result = Calc.Divide(firstOperand, secondOperand);
+                    break;
+                case "%":
+                    result = Calc.RestOfDivide(firstOperand, secondOperand);
+                    break;
+                case "Sqrt":
+                    result = Calc.Sqrt(firstOperand);
+                    break;
+                default:
+                    Console.WriteLine("No such operator!");
+                    break;
+            }
+            Console.WriteLine($"Your result is: {result}");
+        }
+    }
+}
